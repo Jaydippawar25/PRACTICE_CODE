341c6979c8e5ccb2b8f49f99db857baeb47898e5
Jaydippawar25 <jaydippawar2512@gmail.com>
2026-02-09 23:03:22 +0530

    K pattern

---
+++ Java/Pattern/Kpattern.java
@@ -0,0 +1,25 @@
+package Java.Pattern;
+
+import java.util.Scanner;
+
+public class Kpattern {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a numbers of rows:");
+    int num = sc.nextInt();
+
+    for (int i = num; i >= 1; i--) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    for (int i = 2; i <= num; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+}
---
+++ Python/Pattern/KPattern.py
@@ -0,0 +1,7 @@
+num=int(input("Enter a number of rows:"))
+
+for i in range(num,0,-1):
+  print("*"*i) 
+
+for i in range(2,num+1):
+  print("*"*i)
