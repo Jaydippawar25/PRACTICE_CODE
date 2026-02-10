dfa101c56d30589fcc0666b5e2127b3e03420952
Jaydippawar25 <jaydippawar2512@gmail.com>
2026-02-10 13:04:40 +0530

    Update Calculator.py
    modify code

--- Python/Calculator.py
+++ Python/Calculator.py
@@ -1,38 +1,32 @@
 select=input("select the optretor to calculate (+,-,*,/,//,%,**):")
-
-if select=='+':
   num1=int(input("Enter a frist number:"))
   num2=int(input("Enter a secound number:"))
+if select=='+':
+ 
   sum=num1+num2
   print(f"Addition of number{sum}")
 elif select=='-':
-  num1=int(input("Enter a frist number:"))
-  num2=int(input("Enter a secound number:"))
+  
   sub=num1-num2
   print(f"Subraction of number{sub}")
 elif select=='*':
-  num1=int(input("Enter a frist number:"))
-  num2=int(input("Enter a secound number:"))
+  
   mul=num1*num2
   print(f"mulatplication of number{mul}")
 elif select=='/':
-  num1=int(input("Enter a frist number:"))
-  num2=int(input("Enter a secound number:"))
+ 
   div=num1/num2
   print(f"division of number{div:.2f}")
 elif select=='//':
-  num1=int(input("Enter a frist number:"))
-  num2=int(input("Enter a secound number:"))
+ 
   floordiv=num1//num2
   print(f"floor division of number{floordiv:.2f}")
 elif select=='%':
-  num1=int(input("Enter a frist number:"))
-  num2=int(input("Enter a secound number:"))
+  
   modulus=num1%num2
   print(f"Modulus of number{modulus:.2f}")  
 elif select=='**':
-  num1=int(input("Enter a frist number:"))
-  num2=int(input("Enter a secound number:"))
+
   Power=num1**num2
   print(f"Power of two number{Power:.2f}")
 else:
